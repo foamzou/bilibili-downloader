3901a4af966a29551a2652b22cfdf4057e763ce7
Foam Zou <f.zou@aftership.com>
2021-10-10 20:22:02 +0800

    feat: release v0.1

---
+++ index.user.js
@@ -0,0 +1,148 @@
+// ==UserScript==
+// @name         Bilibili downloader
+// @namespace    https://github.com/foamzou/bilibili-downloader
+// @version      0.1
+// @description  哔哩哔哩（b站）音视频下载脚本
+// @author       foamzou
+// @match        https://www.bilibili.com/video/*
+// @icon         https://www.google.com/s2/favicons?domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+var playInfo = null;
+const VIDEO_NAME = 'video.m4s';
+const AUDIO_NAME = 'audio.m4s';
+
+(function() {
+    'use strict';
+    setTimeout(() => {
+        initUI();
+    }, 1300);
+
+})();
+
+function initUI() {
+    createBtn();
+}
+
+function createBtn() {
+    const node = document.createElement('span');
+    node.setAttribute('style', 'width: 70px;');
+    node.setAttribute('class', 'more');
+    node.innerHTML = `<i class="van-icon-download"></i>
+    <div class="more-ops-list" style="width:200px;">
+    <ul>
+    <li id="btnDownloadAudio">下载音频</li>
+    <li id="btnCopyCodeAudio">复制代码：获取音频</li>
+    <li id="btnCopyCodeVideo">复制代码：获取视频</li>
+    </ul></div>`;
+    document.getElementsByClassName('ops')[0].appendChild(node);
+
+    document.getElementById("btnDownloadAudio").addEventListener("click", downloadAudio);
+    document.getElementById("btnCopyCodeAudio").addEventListener("click", copyCodeAudio);
+    document.getElementById("btnCopyCodeVideo").addEventListener("click", copyCodeVideo);
+}
+
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+async function downloadAudio() {
+    if (!document.getElementById('IdDownloadAudio')) {
+        const node = document.createElement('div');
+        node.innerHTML = `<a id="IdDownloadAudio" target="_blank" style="color: #00a3db;font-size: 16px;">音频转码中，请稍等...</a><div></div>`;
+        insertAfter(node, document.getElementById('arc_toolbar_report'));
+    }
+    const aNode = document.getElementById('IdDownloadAudio');
+    const playInfo = getMediaInfo();
+    const responsePayload = await fetch(`https://foamzou.com/tools/bilibili/fetchAudio.php?url=${encodeURIComponent(playInfo.audioUrl)}&name=${playInfo.name}&vid=${playInfo.vid}`);
+    const response = await responsePayload.json();
+    if (response.code != 0) {
+        const tip = '音频转码失败，建议复制代码：获取音频';
+        aNode.text = tip;
+        toast(tip);
+        return;
+    }
+    aNode.href = response.data;
+    toast("音频转码成功，点击链接下载");
+    aNode.text = "音频转码成功，点击下载";
+}
+function copyCode(code) {
+    navigator.clipboard.writeText(code).then(() => {
+        toast('复制到剪贴板，确保本地安装了ffmpeg后，粘贴运行', 900);
+    }).catch(function (err) {
+        toast('复制失败', 900);
+      throw (err !== undefined ? err : new DOMException('The request is not allowed', 'NotAllowedError'))
+    });
+}
+function copyCodeAudio() {
+    copyCode(genMp3Cmd());
+}
+
+function copyCodeVideo() {
+    copyCode(genMp4Cmd());
+}
+
+function getMediaInfo() {
+    if (playInfo !== null) {
+        return playInfo;
+    }
+    const html = document.getElementsByTagName('html')[0].innerHTML;
+    const playinfo = JSON.parse(html.match(/window.__playinfo__=(.+?)<\/script/)[1]);
+
+    playInfo = {
+        videoUrl: playinfo.data.dash.video[0].baseUrl,
+        audioUrl: playinfo.data.dash.audio[0].baseUrl,
+        name: document.title.replace('_哔哩哔哩_bilibili', '').replace(/[ |.|\/]/g, '-'),
+        vid: window.location.href.split('video/')[1].split('?')[0],
+    };
+    return playInfo;
+}
+
+function genMp4Cmd() {
+    const playInfo = getMediaInfo();
+
+    const videoCmd = genCurlCmd(playInfo.videoUrl, VIDEO_NAME);
+    const audioCmd = genCurlCmd(playInfo.audioUrl, AUDIO_NAME);
+    const mp4Cmd = ffmpegMp4(playInfo.name);
+    return `mkdir "${playInfo.name}" ; cd "${playInfo.name}" ; ${videoCmd} ; ${audioCmd} ; ${mp4Cmd}`;
+}
+
+function genMp3Cmd() {
+    const playInfo = getMediaInfo();
+
+    const audioCmd = genCurlCmd(playInfo.audioUrl, AUDIO_NAME);
+    const mp3Cmd = ffmpegMp3(playInfo.name);
+    return `mkdir "${playInfo.name}" ; cd "${playInfo.name}" ; ${audioCmd} ; ${mp3Cmd}`;
+}
+
+function genCurlCmd(url, filename) {
+    return `curl '${url}' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36' \
+  -H 'referer: ${window.location.href}' \
+  --compressed -o ${filename} -v -s`;
+}
+
+function ffmpegMp4(name) {
+    return `ffmpeg -i ${VIDEO_NAME} -i ${AUDIO_NAME}  -c:v copy -strict experimental ${name}.mp4`;
+}
+
+function ffmpegMp3(name) {
+    return `ffmpeg -i ${AUDIO_NAME}  -c:v copy -strict experimental ${name}.mp3`;
+}
+
+function toast(msg, duration) {
+	duration = isNaN(duration) ? 3000 : duration;
+	var m = document.createElement('div');
+	m.innerHTML = msg;
+	m.style.cssText = "font-family:siyuan;max-width:60%;min-width: 150px;padding:0 14px;height: 40px;color: rgb(255, 255, 255);line-height: 40px;text-align: center;border-radius: 4px;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: 999999;background: rgba(0, 0, 0,.7);font-size: 16px;";
+	document.body.appendChild(m);
+	setTimeout(function() {
+		var d = 0.5;
+		m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+		m.style.opacity = '0';
+		setTimeout(function() {
+			document.body.removeChild(m)
+		}, d * 1000);
+	}, duration);
+}
